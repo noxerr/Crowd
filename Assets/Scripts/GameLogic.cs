efa841ce6e4d0b9b585e4cc3f337b9d6748bcb08
Dani Roca <danielrl1994@gmail.com>
2017-05-13 17:10:16 +0200

    first commit

---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLogic : MonoBehaviour {
+
+    [System.Serializable]
+    public class pairGOFloor
+    {
+        public GameObject go;
+        public int floor;
+        public pairGOFloor(GameObject go, int floor)
+        {
+            this.go = go;
+            this.floor = floor;
+        }
+    }
+
+    public GameObject startPoint, finishPoint;
+    public GameObject mainAvatar;
+    public List<pairGOFloor> doorJoints = new List<pairGOFloor>();
+    public bool alarm = false;
+    [Tooltip("1 means no danger, 5 means ship is sunken")]
+    public int levelOfDanger = 1;
+
+    [HideInInspector]
+    public int playerFloor = 4;
+
+    private MindMap userMap;
+    private int stairsLayer;
+    private float lastYStairs;
+
+    // Use this for initialization
+    void Start () {
+        mainAvatar.transform.SetPositionAndRotation(startPoint.transform.position, startPoint.transform.rotation);
+        stairsLayer = LayerMask.NameToLayer("stairs");
+        lastYStairs = startPoint.transform.position.y;
+        Invoke("SetAlarm", 3f );
+        mainAvatar.GetComponent<PlayerLogic>().OnCollEnter += OnPlayerCollided;
+
+    }
+
+    public void AddJoint(GameObject joint, int floor)
+    {
+        doorJoints.Add(new pairGOFloor(joint, floor));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (!alarm)
+        {
+
+        }
+        else
+        {
+
+        }
+	}
+
+
+    private void SetAlarm()
+    {
+        alarm = true;
+    }
+
+
+    private void OnPlayerCollided(Collider collider)
+    {
+        GameObject collidedGO = collider.gameObject;
+        if (collidedGO.layer == stairsLayer)
+        {
+            if (lastYStairs + 1 < collidedGO.transform.position.y) { playerFloor += 1; ChangeFloor(); }
+            else if (lastYStairs - 1 > collidedGO.transform.position.y) { playerFloor -= 1; ChangeFloor(); }
+            lastYStairs = collidedGO.transform.position.y;
+        }
+    }
+
+    private void ChangeFloor()
+    {
+        Debug.Log("Floor number: " + playerFloor);
+    }
+
+}
---
+++ Assets/Scripts/StartCamera.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartCamera : MonoBehaviour {
+    public GameObject ListaPasos, Camera;
+
+    [HideInInspector]
+    public bool activated = true;
+    private Transform[] coordenadas;
+    private Vector3 step;
+    private int iteracion = 0;
+    private float elapsedTime = 2f;
+	// Use this for initialization
+	void Start () {
+        coordenadas = ListaPasos.GetComponentsInChildren<Transform>();
+        step = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (activated) {
+            elapsedTime -= Time.deltaTime;
+            if (elapsedTime < 0) {
+                iteracion++;
+                if (iteracion < coordenadas.Length)
+                {
+                    elapsedTime = 1.7f;
+                    //step = (coordenadas[iteracion].position - coordenadas[iteracion - 1].position) / elapsedTime;
+                    step = (coordenadas[iteracion].position - Camera.transform.position) / elapsedTime;
+                }
+                else
+                {
+                    //GetComponentInParent<Drive>().hasBroom = true;
+                    //GameObject.FindGameObjectWithTag("Command").GetComponent<Commands>().startRecCommand("up");
+                    activated = false;
+                }
+            }
+            else {
+                Camera.transform.position += step * Time.deltaTime;
+            }
+        }
+	}
+}
