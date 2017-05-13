c028b6cad0ffc9687315ee0e2a7122da7f684517
Dani Roca <danielrl1994@gmail.com>
2017-05-13 20:38:42 +0200

    moved scripts into correct folder and added movement of the player to playerlogic

---
+++ Assets/Scripts/PlayerLogic.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLogic : MonoBehaviour {
+
+    public Action<Collider> OnCollEnter;
+    private Collider lastCollider;
+    private Animator myAnimator;
+
+    // Use this for initialization
+    void Start () {
+        myAnimator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        myAnimator.SetFloat("VSpeed", Input.GetAxis("Vertical"));
+        //myAnimator.SetFloat("HSpeed", Input.GetAxis("Horizontal"));
+        if (Input.GetAxis("Horizontal") != 0 && Input.GetAxis("Vertical") != 0)
+        {
+
+            if (Input.GetAxis("Horizontal") > 0) transform.Rotate(Vector3.up * Time.deltaTime * 100f);
+            else transform.Rotate(Vector3.up * Time.deltaTime * -100f);
+        }
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (lastCollider != other)
+        {
+            lastCollider = other;
+            if (OnCollEnter != null) OnCollEnter(other);
+        }
+    }
+}
