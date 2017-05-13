f54a7081343a015672348c36ce2a55ca094dc2bf
Dani Roca <danielrl1994@gmail.com>
2017-05-13 17:19:58 +0200

    fuck git

---
+++ Assets/AddJoint.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddJoint : MonoBehaviour {
+    public GameLogic gl;
+    public int floor;
+	// Use this for initialization
+	void Start () {
+        GameObject go = GameObject.FindGameObjectWithTag("GameLogic");
+        transform.SetParent(go.transform);
+        if (gl == null) gl = go.GetComponent<GameLogic>();
+        gl.AddJoint(gameObject, floor);
+        //gl.doorJoints.Add(gameObject);
+        Destroy(this);
+	}
+	
+}
---
+++ Assets/PlayerLogic.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLogic : MonoBehaviour {
+
+    public Action<Collider> OnCollEnter;
+    private Collider lastCollider;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (lastCollider != other)
+        {
+            lastCollider = other;
+            if (OnCollEnter != null) OnCollEnter(other);
+        }
+    }
+}
