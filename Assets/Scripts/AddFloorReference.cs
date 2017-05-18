64e0c7104b1c275fd90a9037c01cdba0e19fdbe5
Dani Roca <danielrl1994@gmail.com>
2017-05-18 20:53:15 +0200

    moved scripts

---
+++ Assets/Scripts/AddFloorReference.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddFloorReference : MonoBehaviour {
+
+    private GameLogic gl;
+    public int floor;
+    private bool alreadySet = false;
+
+    void Start()
+    {
+        GameObject go = GameObject.FindGameObjectWithTag("GameLogic");
+        transform.SetParent(go.transform);
+        if (gl == null) gl = go.GetComponent<GameLogic>();
+
+    }
+
+    private void LateUpdate()
+    {
+        if (gl.floorContent.Length > 0 && !alreadySet)
+        {
+            gl.SetFloorParent(gameObject, floor);
+            alreadySet = true;
+            Destroy(this);
+        }
+    }
+
+}
